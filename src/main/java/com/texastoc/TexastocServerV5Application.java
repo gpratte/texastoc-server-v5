f5c00fbca56214a6087072de88a16a770a9d2497
Gil Pratte <gil.pratte@gmail.com>
2023-08-19 15:24:28 -0500

    spring initializer generated starter project

---
+++ src/main/java/com/texastoc/TexastocServerV5Application.java
@@ -0,0 +1,13 @@
+package com.texastoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexastocServerV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexastocServerV5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/texastoc/TexastocServerV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texastoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexastocServerV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
