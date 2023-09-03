cd8fb49d9380dca3b47116fae793b413d2126c15
Gil Pratte <gil.pratte@gmail.com>
2023-09-02 19:32:08 -0500

    resurrect the clock service

--- server/src/main/java/com/texastoc/module/clock/event/GameFinalizedHandler.java
+++ server/src/main/java/com/texastoc/module/clock/event/GameFinalizedHandler.java
@@ -1,18 +1,17 @@
 package com.texastoc.module.clock.event;
 
 import com.texastoc.common.GameFinalizedEvent;
+import com.texastoc.module.clock.service.ClockService;
 
 public class GameFinalizedHandler {
 
-  //;;!! private final ClockService clockService;
+  private final ClockService clockService;
 
-  //  public GameFinalizedHandler(ClockService clockService) {
-//    this.clockService = clockService;
-//  }
-  public GameFinalizedHandler() {
+  public GameFinalizedHandler(ClockService clockService) {
+    this.clockService = clockService;
   }
 
   public void handleGameFinalized(GameFinalizedEvent event) {
-    // ;;!! clockService.endClock(event.getGameId());
+    clockService.endClock(event.getGameId());
   }
 }
